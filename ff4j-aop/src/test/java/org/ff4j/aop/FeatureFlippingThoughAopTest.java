97a30ad7c82dbf5a88ba8639eca3040d3b7b6548
nordstrj <jakob.nordstrom@theaa.com>
2015-09-25 16:28:12 +0100

    Support for all spring stereotypes and nested advice.

--- ff4j-aop/src/test/java/org/ff4j/aop/FeatureFlippingThoughAopTest.java
+++ ff4j-aop/src/test/java/org/ff4j/aop/FeatureFlippingThoughAopTest.java
@@ -10,7 +10,7 @@ package org.ff4j.aop;
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * 
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,10 +20,10 @@ package org.ff4j.aop;
  * #L%
  */
 
-import org.ff4j.core.FlippingExecutionContext;
-import org.junit.Assert;
-
 import org.ff4j.FF4j;
+import org.ff4j.core.FlippingExecutionContext;
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -81,4 +81,8 @@ public class FeatureFlippingThoughAopTest {
         Assert.assertTrue(context.sayHelloWithThreadLocal("CLU").startsWith("Bonjour"));
     }
 
+    @After
+    public void disable() {
+        ff4j.disable("language-french");
+    }
 }
