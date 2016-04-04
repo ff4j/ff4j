f74e90dc8c02063c63210245e9beaa89247d9de3
Paul <paul58914080@gmail.com>
2016-04-04 07:05:09 +0530

    Merging ff4j-spring-boot-starter

---
+++ ff4j-spring-boot-starter/src/main/java/org/ff4j/spring/boot/services/MonitoringService.java
@@ -0,0 +1,53 @@
+/*
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2013-2016 the original author or authors.
+ */
+
+package org.ff4j.spring.boot.services;
+
+/*
+ * #%L
+ * ff4j-spring-boot-starter
+ * %%
+ * Copyright (C) 2013 - 2016 FF4J
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.ff4j.FF4j;
+import org.ff4j.spring.boot.domain.EventRepositoryApiBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author <a href="mailto:paul58914080@gmail.com">Paul Williams</a>
+ */
+@Service
+public class MonitoringService {
+    @Autowired
+    private FF4j ff4j;
+
+    public EventRepositoryApiBean getMonitoringStatus(Long start, Long end) {
+        return new EventRepositoryApiBean(ff4j.getEventRepository(), start, end);
+    }
+}
