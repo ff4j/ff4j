8d048408e914ec1b051b8417701333bf66a05a91
Paul <paul58914080@gmail.com>
2016-06-07 23:15:41 +0530

    Fix for the failing tests after the merge request #139

--- ff4j-spring-services/src/main/java/org/ff4j/services/domain/PropertyApiBean.java
+++ ff4j-spring-services/src/main/java/org/ff4j/services/domain/PropertyApiBean.java
@@ -9,9 +9,9 @@ package org.ff4j.services.domain;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,7 +26,7 @@ import org.ff4j.property.Property;
 import org.ff4j.property.util.PropertyFactory;
 
 import java.io.Serializable;
-import java.util.Set;
+import java.util.HashSet;
 import java.util.stream.Collectors;
 
 /**
@@ -46,6 +46,7 @@ public class PropertyApiBean extends BasePropertyBean implements Serializable {
             this.description = property.getDescription();
             this.type = property.getType();
             this.value = property.asString();
+            this.fixedValues = new HashSet<>();
             if (property.getFixedValues() != null) {
                 fixedValues.addAll(property.getFixedValues().stream().map(Object::toString).collect(Collectors.toList()));
             }
