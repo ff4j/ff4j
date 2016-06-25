296bbc11cd2400e67e38078d331bbc3400e5a88e
Paul <paul58914080@gmail.com>
2016-06-25 08:25:36 +0530

    ff4j-spring-boot-starter would not start due to circular reference #132

--- ff4j-spring-boot-autoconfigure/src/main/java/org/ff4j/spring/boot/autoconfigure/FF4JWebConfiguration.java
+++ ff4j-spring-boot-autoconfigure/src/main/java/org/ff4j/spring/boot/autoconfigure/FF4JWebConfiguration.java
@@ -23,9 +23,9 @@ package org.ff4j.spring.boot.autoconfigure;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -36,7 +36,8 @@ package org.ff4j.spring.boot.autoconfigure;
 
 import org.ff4j.FF4j;
 import org.ff4j.web.embedded.ConsoleServlet;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
@@ -50,27 +51,20 @@ import org.springframework.context.annotation.Configuration;
  * @author <a href="mailto:paul58914080@gmail.com">Paul Williams</a>
  */
 @Configuration
-@ConditionalOnClass({FF4j.class, ConsoleServlet.class})
+@ConditionalOnClass({ConsoleServlet.class})
+@AutoConfigureAfter(FF4JConfiguration.class)
 public class FF4JWebConfiguration extends SpringBootServletInitializer {
 
-    @Autowired
-    private FF4j ff4j;
-
-    @Autowired
-    private ConsoleServlet ff4jServlet;
-
     @Bean
-    @ConditionalOnMissingBean
-    public ServletRegistrationBean servletRegistrationBean() {
-        return new ServletRegistrationBean(ff4jServlet, "/ff4j-console");
+    public ServletRegistrationBean servletRegistrationBean(ConsoleServlet ff4jConsoleServlet) {
+        return new ServletRegistrationBean(ff4jConsoleServlet, "/ff4j-console");
     }
 
     @Bean
     @ConditionalOnMissingBean
-    public ConsoleServlet getFF4JServlet() {
-        ConsoleServlet cs = new ConsoleServlet();
-        cs.setFf4j(ff4j);
-        return cs;
+    public ConsoleServlet getFF4jServlet(FF4j ff4j) {
+        ConsoleServlet ff4jConsoleServlet = new ConsoleServlet();
+        ff4jConsoleServlet.setFf4j(ff4j);
+        return ff4jConsoleServlet;
     }
-
 }
