bc4e40083b400cdd64778265208188b89025a571
clun <cedrick.lunven@gmail.com>
2023-01-02 20:40:04 +0100

    cleanup

--- ff4j-cli/src/license/header.ftl
+++
@@ -1,2 +0,0 @@
-${file.name} (${projectName}) by Cedrick LUNVEN
-
--- ff4j-web/src/main/resources/static/js/base.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-	$('.subnavbar').find ('li').each (function (i) {
-		var mod = i % 3;
-		if (mod === 2) {
-			$(this).addClass ('subnavbar-open-right');
-		}
-	})
-});
