68df6e2155a4fcb9b4f75c27245c1565e9159d80
Luis Duarte <1164534+driverpt@users.noreply.github.com>
2025-12-23 11:33:32 +0000

    Fix Terracotta Tests

--- ff4j-store-ehcache/src/test/java/org/ff4j/terracotta/it/FeatureStoreTerracottaTestIT.java
+++ ff4j-store-ehcache/src/test/java/org/ff4j/terracotta/it/FeatureStoreTerracottaTestIT.java
@@ -20,8 +20,14 @@ package org.ff4j.terracotta.it;
  * #L%
  */
 
-import java.util.Map;
-
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.PortBinding;
+import net.sf.ehcache.config.CacheConfiguration;
+import net.sf.ehcache.config.Configuration;
+import net.sf.ehcache.config.MemoryUnit;
+import net.sf.ehcache.config.TerracottaClientConfiguration;
+import net.sf.ehcache.config.TerracottaConfiguration;
+import net.sf.ehcache.terracotta.TerracottaCacheCluster;
 import org.ff4j.core.Feature;
 import org.ff4j.core.FeatureStore;
 import org.ff4j.ehcache.FF4jEhCacheWrapper;
@@ -29,39 +35,70 @@ import org.ff4j.store.FeatureStoreEhCache;
 import org.ff4j.test.store.FeatureStoreTestSupport;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Disabled;
-import net.sf.ehcache.config.CacheConfiguration;
-import net.sf.ehcache.config.Configuration;
-import net.sf.ehcache.config.MemoryUnit;
-import net.sf.ehcache.config.TerracottaClientConfiguration;
-import net.sf.ehcache.config.TerracottaConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.Map;
 
 /**
  * Test to work with Redis as a store.
- * 
+ *
  * @author <a href="mailto:cedrick.lunven@gmail.com">Cedrick LUNVEN</a>
- * 
+ *
  * Working but quite slow to test all on each build
  */
 @Disabled
+@Testcontainers
 public class FeatureStoreTerracottaTestIT extends FeatureStoreTestSupport {
 
-    /** Terracotta URL. */
-    private static final String TERRACOTTA_URL = "localhost:9510";
-    
+    private static final Logger log = LoggerFactory.getLogger(FeatureStoreTerracottaTestIT.class);
+
+    private static final int TERRACOTTA_PORT = 9510;
+    private static final int TERRACOTTA_SYNC_PORT = 9530;
+
+    @Container
+    private static final GenericContainer<?> terracottaContainer =
+            new GenericContainer<>("terracotta/terracotta-server-oss:4.3.5")
+                    .withCreateContainerCmdModifier(it -> it
+                            .withHostName("localhost")
+                            .getHostConfig()
+                            .withPortBindings(
+                                    new PortBinding(
+                                            com.github.dockerjava.api.model.Ports.Binding.bindPort(TERRACOTTA_PORT),
+                                            new ExposedPort(TERRACOTTA_PORT)
+                                    ),
+                                    new PortBinding(
+                                            com.github.dockerjava.api.model.Ports.Binding.bindPort(TERRACOTTA_SYNC_PORT),
+                                            new ExposedPort(TERRACOTTA_SYNC_PORT)
+                                    )
+                            )
+                            .withAutoRemove(true)
+                    )
+                    // This is important so that Ryuk kills this container even if you have this feature enabled
+                    .withReuse(false);
+
+
     /** {@inheritDoc} */
     @Override
     protected FeatureStore initStore() {
-        
-        // Configuration to wirk with Terracotta
+        // Configuration to work with Terracotta
+        String terracottaUrl = String.format("localhost:%d", TERRACOTTA_PORT);
+        log.info("Terracotta server URL={} ", terracottaUrl);
+
         Configuration managerConfiguration = new Configuration();
         managerConfiguration.name("config")
-            
-            .terracotta(new TerracottaClientConfiguration().url(TERRACOTTA_URL))
-            
+            .terracotta(new TerracottaClientConfiguration()
+                    .url(terracottaUrl)
+                    .wanEnabledTSA(false)
+            )
+
             .defaultCache(new CacheConfiguration()
                 .maxBytesLocalHeap(128, MemoryUnit.MEGABYTES)
                 .terracotta(new TerracottaConfiguration()))
-            
+
             .cache(new CacheConfiguration()
                 .name(FF4jEhCacheWrapper.CACHENAME_FEATURES)
                 .maxBytesLocalHeap(128, MemoryUnit.MEGABYTES)
@@ -70,6 +107,7 @@ public class FeatureStoreTerracottaTestIT extends FeatureStoreTestSupport {
 
         FeatureStoreEhCache ehcacheStore = new FeatureStoreEhCache(managerConfiguration);
         ehcacheStore.importFeaturesFromXmlFile("ff4j.xml");
+
         return ehcacheStore;
     }
 
